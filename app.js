3de16d3161fe308cd11a0c21e1866670660b8ed1
YukyNawanjana <yasirunawanjana70@gmail.com>
2020-08-18 22:08:31 +0530

    Html content add

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+const htmlContent = document.querySelector('.html-content');
+
+
+evenetListener();
+
+function evenetListener(){
+
+    document.addEventListener('DOMContentLoaded',function(){
+        // create Html Content 
+        const html = `
+                        <h1 class="text-danger text-center">LOVE CALCULATOR</h1>
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12 mt-5">
+                                <form>
+                                    <div class="form-row">
+                                    <div class="form-group col-md-12">
+                                        <input type="email" class="form-control" id="inputEmail4" placeholder="Boy Name">
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                        <input type="text" class="form-control" id="girlname" placeholder="Girl Name">
+                                    </div>
+                                    </div>
+                                    
+                                    <button type="submit" class="btn btn-danger btn-block mt-5">Calculat</button>
+                                </form>
+                            </div>
+                        </div> 
+                    `;
+          
+        htmlContent.innerHTML = html;
+    });
+}
+
+
 
 let xhr = new XMLHttpRequest();
 
